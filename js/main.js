244199aba8496a6694cb7c6919183bb71541afaf
unknown <hasanmahmoudfoad@gmail.com>
2023-02-03 14:22:03 -0800

    finished the desktop navbar and the yellow button

--- js/main.js
+++ js/main.js
@@ -213,37 +213,37 @@ $("#sponsorshipSocialMediaPk").on( "click", toggleCheckSponsorshipSocialMediaPkF
 
 
 function goToWhatsapp() {
-
+  
   let timePk = document.getElementById("timePk").value;
   let logosPk = document.getElementById("logosPk").value;
   let spotsPk = document.getElementById("spotsPk").value;
   let logoScreenPk = document.getElementById("logoScreenPk").value;
-
+  
   let projectsPk = checkboxProjectsValuePk;
-    let numberOfProjects = numberOfProjectsPkFunction();
-
+  let numberOfProjects = numberOfProjectsPkFunction();
+  
   let interviewsPk = checkboxInterviewsValuePk;
-    let numberOfInterviews = numberOfInterviewsPkFunction();
-
+  let numberOfInterviews = numberOfInterviewsPkFunction();
+  
   let eventsPk = checkboxEventsValuePk;
-    let numberOfEvents = numberOfEventsPkFunction();
-
+  let numberOfEvents = numberOfEventsPkFunction();
+  
   let reportagePk = checkboxReportageValuePk;
-    let numberOfReportage = numberOfReportagePkFunction();
-
-
+  let numberOfReportage = numberOfReportagePkFunction();
+  
+  
   let newsPk = checkboxReportageValuePk;
-    let numberOfNews = numberOfNewsPkFunction();
-
+  let numberOfNews = numberOfNewsPkFunction();
+  
   let logoInsideStudioPk = checkboxLogoInsideStudioValuePk;
   let logoOutdoorStudioPk = checkboxLogoOutdoorStudioValuePk;
   let SponsorshipEventsPk = checkboxSponsorshipEventsValuePk;
   let sponsorshipSocialMedia = checkboxSponsorshipSocialMediasValuePk;
-
-
-
-
-  let url = `https://wa.me/+201153380655?text=
+  
+  
+  
+  
+  let url = `https://wa.me/+201288009558?text=
   Time In Minutes:%20  ${timePk}%0a
   Logos :%20  ${logosPk}%0a
   Spots :%20   ${spotsPk}%0a
@@ -263,17 +263,22 @@ function goToWhatsapp() {
   Sponsorship Masaa Masr events :%20  ${SponsorshipEventsPk}%0a
   Sponsorship Masaa Masr social media :%20  ${sponsorshipSocialMedia}%0a
   `;
-
+  
   window.open(url, '_blank').focus();
 }
 
- 
+
 
 
 
 
 $(document).ready(function () {
-
+  
+  
+  // adding a  unique class for the scroll spy
+  $('section').addClass('unique');
+  // adding a  unique class for the scroll spy
+  
   /*** pre loader  ***/
   preloaderFadeOutTime = 700;
   function hidePreloader() {
@@ -283,8 +288,8 @@ $(document).ready(function () {
   } hidePreloader();
   $('.load-layer').fadeOut();
   /*** pre loader  ***/
-
-
+  
+  
   /**** Pc nav ****/
   $(' nav li a').click(function () {
     $('html , body').animate({
@@ -292,8 +297,8 @@ $(document).ready(function () {
     }, 2000);
   });
   /**** Pc nav ****/
-
-
+  
+  
   /***  Mobile nav ***/
   $(' navbar li a').click(function () {
     $('html , body').animate({
@@ -304,9 +309,9 @@ $(document).ready(function () {
     $(".nav-toggle").click();
   });
   /***  Mobile nav ***/
-
-
-
+  
+  
+  
   /* into Button */
   $('#home .big-btn').click(function () {
     $('html , body').animate({
@@ -314,48 +319,65 @@ $(document).ready(function () {
     }, 2000);
   });
   /* into Button */
-
-
-
+  
+  
+  
   $("#go-top").on("click", function () {
     $('html, body').animate({ scrollTop: 0 }, 2000);
   });
-
-
-
+  
+  
+  
   /*scrolling events */
   /*scrolling events */
-
+  
   $(window).scroll(function () {
-
-    //nav color scroll change
+    
+    // START nav color scroll change
+    /*
     if ($(this).scrollTop() > 30) {
       $('nav').addClass('bg-light');
     } else {
       $('nav').removeClass('bg-light');
     }
-
-    //go to top buttom
+    */
+    // END nav color scroll change
+    
+    
+    
+    // START go to top button
     if ($(this).scrollTop() > 650) {
       $('#go-top').fadeIn();
     } else {
       $('#go-top').fadeOut();
     }
-
+    //END go to top button
+    
+    
+    
+    // START Scroll Spy Adjustments
+    $('.unique').each(function(){
+      var uniqueClass = $(this).attr('id');
+      if($(window).scrollTop() > $(this).offset().top -200) {
+        $('#wrapper li a').removeClass('active');
+        $('#wrapper li a[data-scrolling = "' + uniqueClass + '"]').addClass('active');
+      }
+    });
+    // END Scroll Spy Adjustments
+    
   });
-
-
-
-
-  //go to top buttom
-
+  
+  
+  //go to top button
+  
+  
+  // START adjustments on the window object
   let win = $(window);
-
+  
   if (win.width() <= 991) {
     colReverse();
   };
-
-
+  
   if (win.width() >= 1249) {
     $('#my-img').enllax();
     $('#min-projects').enllax();
@@ -364,9 +386,9 @@ $(document).ready(function () {
     let doubledProjectHeight = currentProjectHeight * 1.3;
     $("#min-projects").height(doubledProjectHeight);
   };
-
-
-
+  // END adjustments on the window object
+  
+  
   
   //toggle password SHOW/HIDE 
   $("#togglePassword").on("click", function () {
@@ -377,11 +399,11 @@ $(document).ready(function () {
       passInput.type = "password";
     }
   });
-  // password SHOW/HIDE ----------------------------------------
+  // password SHOW/HIDE 
   
-
-
-  //SHOW Prices if the password is correct
+  
+  
+  // START showing the Prices if the password is correct
   const pricingButton = $("#getPrices");
   const passwordAuth = $(".pass-auth");
   pricingButton.on("click", function () {
@@ -397,38 +419,46 @@ $(document).ready(function () {
       console.log('wrong Password');
     }
   });
-  // show Prices ----------------------------------------
-
-
-
-
-});//end ready
-
-
-
-
+  // END show Prices 
   
+  
+  //START Img button  hover 
+  $(".y-btn").hover(function(){
+    $(this).find("span").css("letter-spacing", "2px");
+  }, function(){
+    $(this).find("span").css("letter-spacing", "0px");
+  });
+  //End Img button  hover
+  
+  
+  
+});//end ready==============================================================
+
+
+
+
+
 
 
 
 // Start the Hamburger Menu  
 (function () {
-
+  
   var hamburger = {
     navToggle: document.querySelector('.nav-toggle'),
     nav: document.querySelector('navbar'),
-
+    
     doToggle: function (e) {
       e.preventDefault();
       this.navToggle.classList.toggle('expanded');
       this.nav.classList.toggle('expanded');
     }
   };
-
+  
   hamburger.navToggle.addEventListener('click', function (e) { hamburger.doToggle(e); });
-
+  
 }());
-// Hamburger----------------------------------------
+// END the Hamburger Menu  
 
 
 
@@ -438,30 +468,30 @@ $(document).ready(function () {
 
 
 
-  // ----------------------------------------
-  /* (() => {
-    const theme = window.localStorage.getItem("theme");
-    if (theme == "Light") {
-      document.querySelector("html").style = "--dark-purple:#1ccf50; --light-blue:#09b3b4; --dark-navey:#fff;"
-    }
-  })();
-  
-  
-  document.querySelector("#toggleTheme").addEventListener("click", themeToggle);
-  
-  function themeToggle() {
-    
-    const current = window.localStorage.getItem("theme");
-    
-    if (current == "Light") {
-      window.localStorage.setItem("theme", "Dark");
-    } else {
-      window.localStorage.setItem("theme", "Light");
-    }
-    //console.log(current);
+// ----------------------------------------
+/* (() => {
+  const theme = window.localStorage.getItem("theme");
+  if (theme == "Light") {
+    document.querySelector("html").style = "--dark-purple:#1ccf50; --light-blue:#09b3b4; --dark-navey:#fff;"
   }
-  */
-  // ----------------------------------------
+})();
+
+
+document.querySelector("#toggleTheme").addEventListener("click", themeToggle);
+
+function themeToggle() {
+  
+  const current = window.localStorage.getItem("theme");
+  
+  if (current == "Light") {
+    window.localStorage.setItem("theme", "Dark");
+  } else {
+    window.localStorage.setItem("theme", "Light");
+  }
+  //console.log(current);
+}
+*/
+// ----------------------------------------
 
 
 
