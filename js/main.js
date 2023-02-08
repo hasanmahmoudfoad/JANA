950557c1ed1d689a5d6efc73c9e7f18833a8098a
unknown <hasanmahmoudfoad@gmail.com>
2023-02-08 14:38:43 -0800

    finished implementing the whole theme v.1

--- js/main.js
+++ js/main.js
@@ -9,6 +9,13 @@ function colReverse() {
   console.log('added flex-column-reverse');
 };  
 
+function pushOffers() { 
+  const pricingData = `<section class="pv-md sponsorship"id=sponsorship><div class="container-adjust container-fluid"><div class="new-title relative-div"><div class="tl-left"></div><h2>Sponsorship Offers</h2><div class="tl-right"></div></div><div class="pv-md row wrapper"><div class="col-12 col-lg-4 margin-b"><div class="package-container relative-div"><div class="frame basic"><div class=price-section><h2 class=price>2,400,000</h2><span class=currency>LE.</span></div><div class=title-basic><h3 class="p-3 basic-color">Basic</h3></div><div class="table basic"><div class=features><li><span class=list-name><b><span class=plan-color>100</span> </b>Minutes. </span><span class="check icon"><img src=./images/check-mark.png></span><li><div class="list-name mt-4"><strong>Sponsorship masaa masr</strong><ul class=sub-list-name><li>Logos.<span class="check icon"><img src=./images/check-mark.png></span><li>Spots.<span class="check icon"><img src=./images/check-mark.png></span></ul></div><li><span class=list-name><span class=plan-color>208</span> Logo screen Masaa Masr studio. </span><span class="check icon"><img src=./images/check-mark.png></span><li><div class="list-name mt-4"><strong>100 minutes including :</strong><ul class=sub-list-name><li><div><span class=list-name></span>coverage & review <span class=plan-color>1</span> project</div><span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>interviews.</span><span class="check icon"><img src=./images/check-mark.png></span><li><div><span class=list-name>coverage</span> <span class=plan-color>1</span> event</div><span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Reportage.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>News.</span> <span class="check icon"><img src=./images/check-mark.png></span></ul></div><li><span class=list-name>Logo screen masaa masr events.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Sponsorship Masaa Masr social media. </span><span class="check icon"><img src=./images/check-mark.png></span></div></div></div></div></div><div class="col-12 col-lg-4 margin-b"><div class="package-container relative-div"><div class="frame standard"><div class=price-section><h2 class=price>3,200,000</h2><span class=currency>LE.</span></div><div class=title-basic><h3 class="p-3 standard-color">Standard</h3></div><div class="table standard"><div class=features><li><span class=list-name><b><span class=plan-color>180</span> </b>Minutes.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><div class="list-name mt-4"><strong>Sponsorship masaa masr :</strong><ul class=sub-list-name><li><div><span class=list-name>Logos</span> <span class=plan-color>1920</span></div><span class="check icon"><img src=./images/check-mark.png></span><li><div><span class=list-name>Spots</span> <span class=plan-color>553</span></div><span class="check icon"><img src=./images/check-mark.png></span></ul></div><li><div><span class=plan-color>416 </span><span class=list-name>Logo screen Masaa Masr studio.</span></div><span class="check icon"><img src=./images/check-mark.png></span><li><div class=list-name><strong>180 minutes including :</strong><ul class=sub-list-name><li><div><span class=list-name>Coverage & review</span> <span class=plan-color>2 </span>projects.</div><span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Interviews.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><div><span class=list-name>Covering </span><span class=plan-color>2 </span>events.</div><span class="check icon"><img src=./images/check-mark.png></span><li><div><span class=plan-color>2 </span><span class=list-name>segment reportage.</span></div><span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>News.</span> <span class="check icon"><img src=./images/check-mark.png></span></ul></div><li><span class=list-name>Sponsorship Masaa Masr events. </span><span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Sponsorship Masaa Masr social media. </span><span class="check icon"><img src=./images/check-mark.png></span></div></div></div></div></div><div class="col-12 col-lg-4 margin-b"><div class="package-container relative-div"><div class="frame premium"><div class=price-section><h2 class=price>4,800,000</h2><span class=currency>LE.</span></div><div class=title-basic><h3 class="p-3 premium-color">Premium</h3></div><div class="table Premium"><div class=features><li><div><span class=list-name><b><span class=plan-color>240 </span></b>Minutes.</span></div><span class="check icon"><img src=./images/check-mark.png></span><li><div class="list-name mt-4"><strong>Sponsorship masaa masr :</strong><ul class=sub-list-name><li><div><span class=list-name>Logos</span> <span class=plan-color>2880</span></div><span class="check icon"><img src=./images/check-mark.png></span><li><div><span class=list-name>Spots</span> <span class=plan-color>830</span></div><span class="check icon"><img src=./images/check-mark.png></span></ul></div><li><div><span class=list-name><span class=plan-color>830</span> Logo screen Masaa Masr studio.</span></div><span class="check icon"><img src=./images/check-mark.png></span><li><div class=list-name><strong>240 minutes including :</strong><ul class=sub-list-name><li><span class=list-name>Coverage & review projects.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Interviews.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Coverage events.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><div><span class=plan-color>4 </span>segment reportage.</div><span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>News.</span> <span class="check icon"><img src=./images/check-mark.png></span></ul></div><li><span class=list-name>The company or project logo inside the studio.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>The company or project logo outdoor campaign the studio.</span> <span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Sponsorship masaa masr events. </span><span class="check icon"><img src=./images/check-mark.png></span><li><span class=list-name>Sponsorship Masaa Masr social media. </span><span class="check icon"><img src=./images/check-mark.png></span></div></div></div></div></div><a class=y-btn href=./custom-package.html id=getPrices><img src=./images/long-button.png><span class=font-poppins>Customizable packages</span></a></section>`;
+  const sponsorshipPricing = document.getElementById("pricingHandler");
+  sponsorshipPricing.innerHTML = pricingData;
+  passwordAuth.hide();
+}
+
 // START Coverage & review projects.
 let checkboxProjectsValuePk = "NO"; 
 function toggleCheckboxProjectsFunction(){
@@ -274,10 +281,10 @@ function goToWhatsapp() {
 
 $(document).ready(function () {
   
-  
   // adding a  unique class for the scroll spy
   $('section').addClass('unique');
   // adding a  unique class for the scroll spy
+  
   
   /*** pre loader  ***/
   preloaderFadeOutTime = 700;
@@ -311,39 +318,37 @@ $(document).ready(function () {
   /***  Mobile nav ***/
   
   
-  
-  /* into Button */
-  $('#home .big-btn').click(function () {
-    $('html , body').animate({
+  /***** intro Button ****/
+  $("#home .y-btn").click(function () {
+    console.log("anti ghosting");
+    $("html , body").animate({
       scrollTop: $('#' + $(this).data("scrolling")).offset().top
     }, 2000);
   });
-  /* into Button */
+  /***** intro Button ****/
   
   
-  
+  /***** go to top Button ****/
   $("#go-top").on("click", function () {
     $('html, body').animate({ scrollTop: 0 }, 2000);
   });
+  /***** go to top Button ****/
   
   
   
   /*scrolling events */
   /*scrolling events */
-  
+  /*scrolling events */
+  /*scrolling events */
   $(window).scroll(function () {
     
     // START nav color scroll change
-    /*
     if ($(this).scrollTop() > 30) {
-      $('nav').css({"box-shadow ":"0px 1px 15px #eee"});
+      $('nav').addClass("nav-shadow");
     } else {
-      $('nav').css({"box-shadow ":"none"});
+      $('nav').removeClass("nav-shadow");
     }
-    */
-    
     // END nav color scroll change
-    
     
     
     // START go to top button
@@ -353,7 +358,6 @@ $(document).ready(function () {
       $('#go-top').hide();
     }
     //END go to top button
-    
     
     
     // START Scroll Spy Adjustments
@@ -367,9 +371,12 @@ $(document).ready(function () {
     // END Scroll Spy Adjustments
     
   });
+  /*scrolling events */
+  /*scrolling events */
+  /*scrolling events */
+  /*scrolling events */
   
   
-  //go to top button
   
   
   // START adjustments on the window object
@@ -385,7 +392,6 @@ $(document).ready(function () {
   // END adjustments on the window object
   
   
-  
   //toggle password SHOW/HIDE 
   $("#togglePassword").on("click", function () {
     let passInput = document.getElementById("passwordInput");
@@ -395,27 +401,47 @@ $(document).ready(function () {
       passInput.type = "password";
     }
   });
-  // password SHOW/HIDE 
+  // password SHOW/HIDE  
   
   
+  // START showing the Prices if the password is correct------------------------
+  pricingButton = $("#getPrices");
+  passwordAuth = $(".pass-auth");
+  passwordInput = document.getElementById("passwordInput");
   
-  // START showing the Prices if the password is correct
-  const pricingButton = $("#getPrices");
-  const passwordAuth = $(".pass-auth");
-  pricingButton.on("click", function () {
-    // if user enter the correct password, then render the  <<< pricingData >>>
-    let passwordInput = document.getElementById("passwordInput");
+  // START function Triggered by clicking the button
+  pricingButton.on( "click", function () {
+    // if user enter the correct password, then pushOffers();
     if (passwordInput.value == "janaPRC") {
-      const pricingData = `<section id="sponsorship" class="sponsorship pv-md"> <div class="container-adjust container-fluid"> <h2 class="font-Montserrat glow py-5 text-center">Sponsorship Offers</h2> <div class="row wrapper"> <div class="col-12 col-lg-4 margin-b"> <div class=frame> <div class="table basic"> <div class=price-section> <h2 class=price>2,400,000</h2><span class=currency>LE.</span> </div><div class=features> <li><span class=list-name><b><span class=plan-color>100</span> </b>Minutes. </span><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div class="list-name mt-4"><strong>Sponsorship masaa masr</strong> <ul class=sub-list-name> <li>Logos.<span class="check icon"><i class="fa-check-circle fas"></i></span> <li>Spots.<span class="check icon"><i class="fa-check-circle fas"></i></span> </ul> </div><li><span class=list-name><span class=plan-color>208</span> Logo screen Masaa Masr studio. </span><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div class="list-name mt-4"><strong>100 minutes including :</strong> <ul class=sub-list-name> <li> <div><span class=list-name></span>coverage & review <span class=plan-color>1</span> project</div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>interviews.</span><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div><span class=list-name>coverage</span> <span class=plan-color>1</span> event</div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Reportage.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>News.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> </ul> </div><li><span class=list-name>Logo screen masaa masr events.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Sponsorship Masaa Masr social media. </span><span class="check icon"><i class="fa-check-circle fas"></i></span> </div></div></div></div><div class="col-12 col-lg-4 margin-b"> <div class=frame> <div class="table standard"> <div class=price-section> <h2 class=price>3,200,000</h2><span class=currency>LE.</span> </div><div class=features> <li><span class=list-name><b><span class=plan-color>180</span> </b>Minutes.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div class="list-name mt-4"><strong>Sponsorship masaa masr :</strong> <ul class=sub-list-name> <li> <div><span class=list-name>Logos</span> <span class=plan-color>1920</span> </div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div><span class=list-name>Spots</span> <span class=plan-color>553</span> </div><span class="check icon"><i class="fa-check-circle fas"></i></span> </ul> </div><li> <div><span class=plan-color>416 </span><span class=list-name>Logo screen Masaa Masr studio.</span></div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div class=list-name><strong>180 minutes including :</strong> <ul class=sub-list-name> <li> <div><span class=list-name>Coverage & review</span> <span class=plan-color>2 </span>projects.</div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Interviews.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div><span class=list-name>Covering </span><span class=plan-color>2 </span>events.</div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div><span class=plan-color>2 </span><span class=list-name>segment reportage.</span></div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>News.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> </ul> </div><li><span class=list-name>Sponsorship Masaa Masr events. </span><span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Sponsorship Masaa Masr social media. </span><span class="check icon"><i class="fa-check-circle fas"></i></span> </div></div></div></div><div class="col-12 col-lg-4 margin-b"> <div class=frame> <div class="table Premium"> <div class=price-section> <h2 class=price>4,800,000</h2><span class=currency>LE.</span> </div><div class=features> <li> <div><span class=list-name><b><span class=plan-color>240 </span></b>Minutes.</span> </div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div class="list-name mt-4"><strong>Sponsorship masaa masr :</strong> <ul class=sub-list-name> <li> <div><span class=list-name>Logos</span> <span class=plan-color>2880</span> </div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div><span class=list-name>Spots</span> <span class=plan-color>830</span> </div><span class="check icon"><i class="fa-check-circle fas"></i></span> </ul> </div><li> <div><span class=list-name><span class=plan-color>830</span> Logo screen Masaa Masr studio.</span> </div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div class=list-name><strong>240 minutes including :</strong> <ul class=sub-list-name> <li><span class=list-name>Coverage & review projects.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Interviews.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Coverage events.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li> <div><span class=plan-color>4 </span>segment reportage.</div><span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>News.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> </ul> </div><li><span class=list-name>The company or project logo inside the studio.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>The company or project logo outdoor campaign the studio.</span> <span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Sponsorship masaa masr events. </span><span class="check icon"><i class="fa-check-circle fas"></i></span> <li><span class=list-name>Sponsorship Masaa Masr social media. </span><span class="check icon"><i class="fa-check-circle fas"></i></span> </div></div></div></div><a href="./custom-package.html" class="big-btn">Customizable package</a> </div></section>`;
-      const sponsorshipPricing = document.getElementById("pricingHandler");
-      sponsorshipPricing.innerHTML = pricingData;
-      passwordAuth.hide();
+      pushOffers();
     } else {
       passwordInput.classList.add("wrong-pass");
       console.log('wrong Password');
     }
   });
-  // END show Prices 
+  // END function Triggered by clicking the button
+  
+  
+  // START function Triggered by Enter Key
+  $("#passwordInput").keypress(function(e){
+    if(e.which == 13){
+      if (passwordInput.value == "janaPRC") {
+        pushOffers();
+      } else {
+        passwordInput.classList.add("wrong-pass");
+        console.log('wrong Password');
+      }
+    }
+  });
+  // END function Triggered by Enter Key
+  
+  
+  //  START remove wrong class when focus on the input
+  $( "#passwordInput" ).focus(function() {
+    passwordInput.classList.remove("wrong-pass");
+  });
+  //  END remove wrong class when focus on the input
+  // END show Prices ----------------------------------------------------------
   
   
   //START Img button  hover 
